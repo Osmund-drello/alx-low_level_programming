fc15f11ec2a25a0b62c68309361f69a501cbd1b0
Osmund-drello <osmunddrello@gmail.com>
2023-03-23 18:38:10 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,18 @@
 
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
 	{
-		putchar('_');
+		putchar('\n');
 	}
-	putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			putchar('_');
+		}
+		putchar('\n');
+	}
 }
