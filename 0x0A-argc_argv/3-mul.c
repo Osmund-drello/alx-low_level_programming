b3b2ade325b2be1b5483c16ff1206d5823624cac
Osmund-drello <osmunddrello@gmail.com>
2023-04-10 04:03:11 -0700

    atoi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,71 +2,25 @@
 #include <stdio.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * main - prints multiplication of two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if true, 1 if false
  */
 
-int _atoi(char *s)
+int main(int argc, char **argv)
 {
-	int i, d, n, len, f, dig;
+	int x, y;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	dig = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	if (argc < 3)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			dig = s[i] - '0';
-			if (d % 2)
-				dig = -dig;
-			n = n * 10 + dig;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+		printf("Error\n");
+		return (1);
 	}
 
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (success), 1 (error)
- */
-
-int main(int argc, char *argv[])
-{
-	int result, num1, num2;
-
-	if (argc < 3 || argc > 3)
-	{
-		printf("error\n");
-		return (0);
-	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
+	x = _atoi(argv[1]);
+	y = _atoi(argv[2]);
+	printf("%d\n", x * y);
 
 	return (0);
 }
